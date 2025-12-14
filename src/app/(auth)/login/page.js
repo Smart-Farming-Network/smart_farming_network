f7f0d9a9946487eb5e17b4186f6846c84ff93d19
Nathaniel Egbodo Odo <egbodonathaniel@yahoo.com>
2025-12-14 04:52:44 +0100

    feat: replace custom auth function with getServerSession for login and register pages

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -1,12 +1,12 @@
-import { auth } from '@/libs/auth';
-import LoginForm from '@/components/auth/LoginForm';
-import { redirect } from 'next/navigation';
-
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/libs/auth";
+import { redirect } from "next/navigation";
+import LoginForm from "@/components/auth/LoginForm";
 
 export default async function LoginPage() {
-  const session = await auth();
+  const session = await getServerSession(authOptions);
+
   if (session) redirect("/");
 
-  
   return <LoginForm />;
 }
